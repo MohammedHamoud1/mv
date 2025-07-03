5ffac1aa6afdac443f6656bc830c05b5d1bf9245
makeen-khawajkeah <makeenkh2004@gmail.com>
2025-07-03 15:32:13 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => {
         </div>
         
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-sm text-gray-400">
-          <p>&copy; 2024 SecureBounty. All rights reserved.</p>
+          <p>&copy; 2025 Damsec. All rights reserved.</p>
         </div>
       </div>
     </footer>
